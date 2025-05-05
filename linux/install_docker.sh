af470fa871f1a76cf504ebb11c02e93720a3adc7
Scuzz50 <ktheos@scuzz.ca>
2025-05-05 01:10:57 -0400

    message

--- linux/install_docker.sh
+++ linux/install_docker.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+#Install docker & docker compose
+apt update -y
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+apt install docker-ce -y
+usermod -aG docker ubuntu
+chmod 666 /var/run/docker.sock
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose

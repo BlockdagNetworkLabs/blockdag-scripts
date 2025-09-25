a69c4ec552a10e7cd849b53ccb30844aa6053415
dev-580 <abhishek.chauhan@antiersolutions.com>
2025-09-25 17:28:50 +0530

    awakening blockdag scripts to run node and nodeworker

--- linux/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf  /home/ubuntu/.dagwallet
-rm -rf /root/.dagwallet
-rm -rf ~/.dagwallet
-rm -rf wallet.txt
--- mac/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -rf  /Users/user/Library/Application\ Support/Dagwallet/
-rm -rf ~/Dagwallet
